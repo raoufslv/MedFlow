50283c050f1bcda2bd9fa0463f4be6c95d8abf7b
Raouf <raoufslv09@gmail.com>
2024-02-26 21:27:36 +0100

    updating the backend URL

--- Frontend/src/config/axiosConfig.js
+++ Frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'https://medflow.onrender.com',
   // other config options if needed
 });
 
